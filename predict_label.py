ecff5ea0ab2d5b0d0a0e1744920f108ec459ce3c
IlyaKusakin <kusakin_ilya@icloud.com>
2021-01-07 13:51:46 +0300

    Added script for label predicting

---
+++ predict_label.py
@@ -0,0 +1,62 @@
+import razdel
+import transformers
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+bert_tokenizer = transformers.BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased')
+tuned_RuBERT = transformers.BertForSequenceClassification.from_pretrained(
+    'DeepPavlov/rubert-base-cased', 
+    output_attentions=True,
+    pad_token_id=bert_tokenizer.eos_token_id,
+    num_labels=2,
+    return_dict = True
+).to(device)
+
+weights_file = 'weights/tuned_RuBERT_common.pt'
+tuned_RuBERT.load_state_dict(torch.load(weights_file))
+
+def filter_text(text):
+    line = razdel.tokenize(text.lower())
+    line = [token.text for token in line]
+    filtered_line = ' '.join([token for token in line if token.isalpha()])
+        
+    return filtered_line
+
+def sample_batch(text, tokenizer=bert_tokenizer, max_length=16):
+    tokenizer_output = tokenizer.encode_plus(
+            filter_text(text), max_length = max_length,
+            return_tensors = 'pt', padding = 'max_length')
+        
+    return {
+            "input_ids": tokenizer_output['input_ids'].squeeze(0)[:max_length], 
+            "mask": tokenizer_output['attention_mask'].squeeze(0)[:max_length],
+           }
+
+def predict(texts):
+    inputs = []
+    mask = []
+    for text in texts:
+        batch = sample_batch(text)
+        inputs.append(batch['input_ids'].numpy().tolist())
+        mask.append(batch['mask'].numpy().tolist())
+
+    inputs = torch.tensor(inputs)
+    mask = torch.tensor(mask)
+    tuned_RuBERT.eval()
+    with torch.no_grad():
+      outputs = tuned_RuBERT(input_ids = inputs.to(device), attention_mask = mask.to(device))
+      probas = torch.nn.functional.softmax(outputs.logits, dim=-1)
+
+    result = []
+    for proba in probas:
+        result.append({"negative": proba.tolist()[0], "positive": proba.tolist()[1]})
+    
+    return result
+
+messages = ['черт я так устала не могу', 'ура я довольный и счастливый']
+
+results = predict(messages)
+for message, sentiment in zip(messages, results):
+    # черт я так устала не могу -> {'negative': 0.985421359539032, 'positive': 0.014578700996935368}
+    # ура я довольный и счатсливый -> {'negative': 0.03474540263414383, 'positive': 0.9652546048164368}
+    print(message, '->', sentiment)
